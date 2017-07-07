366d2c942db18bbe8729a8341084a52d09835e77
ywc2016 <yu_weicheng@buaa.edu.cn>
2017-07-07 12:56:45 +0800

    123

--- src/com/buaa/clazz/Demo1.java
+++ src/com/buaa/clazz/Demo1.java
@@ -13,25 +13,41 @@ import org.omg.CORBA.PUBLIC_MEMBER;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		Manager x = new Manager();
-		x.name = "Jack";
-		x.salary = 5000;
-		System.out.println(x.getDetails());
-
+		Person person = new Person();
+		System.out.println("BMI:" + person.Bmivalue());
+		System.out.println("标准体重:" + person.StdWeight());
+		System.out.println("肥胖度:" + person.ObesityDegree());
 	}
 }
 
-class Employee {
-	public String name;
-	public double salary;
-	public Date birthDate;
+class Person {
+	private String name = "jack";
+	private int age = 22;
+	private double height = 1.77;
+	private double weight = 63;
 
-	public String getDetails() {
-		return name + " " + salary;
+	public double Bmivalue() {
+		return height / (weight * weight);
 	}
 
-}
+	public double StdWeight() {
+		return 22 * height * height;
+	}
 
-class Manager extends Employee {
-	public String department;
+	public double ObesityDegree() {
+		return (weight - StdWeight()) / StdWeight() * 100;
+	}
+
+	public String Conclusion() {
+		if (ObesityDegree() < -10)
+			return "瘦";
+		else if (ObesityDegree() < 20) {
+			if (Bmivalue() < 24.2)
+				return "普通";
+			else
+				return "超重";
+		} else
+			return "肥胖";
+	}
+
 }
